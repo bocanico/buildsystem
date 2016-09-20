f1aa74612dc1bc6165db77d5ad06c9eaae42a61f
bocanico <arnaoult@gmail.com>
2016-09-20 17:39:49 +0200

    loader babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
 var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
+
 module.exports = {
-    entry: "./src/index.jade",
+    entry: {
+        app: ['./src/style/main.scss','./src/js/main.js']
+    },
     output: {
-        filename: "./src/index.html"
+        path: './dist',
+        filename: 'build.js'
     },
     module: {
         loaders: [
-            { test: /\.jade$/, loader: "jade-" }
+            {test: /\.jade$/, loader: 'jade'},
+            {test: /\.scss$/, loader: ExtractTextPlugin.extract("css!sass")},
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a valid name to reference
+                query: {
+                    presets: ['es2015']
+                }
+            }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({filename: 'index.html', template:'./src/index.jade'})
+    ]
 };
